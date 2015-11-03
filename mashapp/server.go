7590808f096a2f6f23e0eeec81efd0e86c034acc
padster <padsterpat@gmail.com>
2015-11-03 13:04:04 -0800

    Initial stubs for mashup app added

---
+++ mashapp/server.go
@@ -0,0 +1,49 @@
+package mashapp
+
+import (
+    "fmt"
+    "html/template"
+    "net/http"    
+)
+
+type MashAppServer struct {
+    port int
+    rootPath string
+}
+
+func NewServer(port int, rootPath string) *MashAppServer {
+    return &MashAppServer{port, rootPath}
+}
+
+func (s *MashAppServer) Serve() {
+    addr := fmt.Sprintf(":%d", s.port)
+    fmt.Printf("Serving http://localhost%s/\n", addr)
+
+    serveStaticFiles(fmt.Sprintf("%s/static", s.rootPath), "static")
+    http.HandleFunc("/", s.appHandler)
+    
+    http.ListenAndServe(addr, nil)
+}
+
+func (s *MashAppServer) appHandler(w http.ResponseWriter, r *http.Request) {
+    s.renderTemplate(w, "app.html", nil)
+}
+
+func (s *MashAppServer) renderTemplate(w http.ResponseWriter, templateName string, data interface{}) {
+    asPath := fmt.Sprintf("%s/%s", s.rootPath, templateName)
+    t, err := template.ParseFiles(asPath)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    err = t.Execute(w, data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func serveStaticFiles(fromDirectory string, toHttpPrefix string) {
+    asPath := fmt.Sprintf("/%s/", toHttpPrefix)
+    fs := http.FileServer(http.Dir(fromDirectory))
+    http.Handle(asPath, http.StripPrefix(asPath, fs))
+}
